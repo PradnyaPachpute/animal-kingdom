f04f427fed290fa5e8512953964891df6562a49e
PradnyaPachpute <pradnya.pachpute@gmail.com>
2020-07-21 10:45:09 +0530

    Added a property which determines chicken as rooster. Modified the sing method implementation based on the isRooster property.

--- src/com/animals/Chicken.java
+++ src/com/animals/Chicken.java
@@ -3,9 +3,22 @@ package com.animals;
 
 public class Chicken extends Birds{
 	
+	private boolean isRooster;
+	
+	public Chicken() {
+		super();
+	}
+	
+	public Chicken(boolean isRooster){
+		this.isRooster=isRooster;
+	}
+	
 	@Override
 	public void sing() {
-		System.out.println("Cluck,cluck");
+		if(!isRooster)
+			System.out.println("Cluck,cluck");
+		else
+			System.out.println("Cock-a-doodle-doo");
 	}
 	
 	@Override
